2aa4827cca403cf9dd7c2a16615c8c1ec46997f9
Celvincy04 <2438710@bdeb.qc.ca>
2025-12-10 13:30:51 -0500

    AnimationEF a moitié fini!

--- src/scripts/etoilesAnimation.js
+++ src/scripts/etoilesAnimation.js
@@ -5,10 +5,40 @@ export default (()=>{
     const element = document.querySelector(".etoileFill01");
 
     gsap.to(element, {
-        x: -400,
-        y: 400,
-        duration: 2,
+        x: -2000,
+        y: 2000,
+        duration: 3,
         delay: 2,
+        repeatDelay: 5,
+        repeat: -1,
+
+
+        onRepeat: () => {
+
+        },
+    })
+    const element2 = document.querySelector(".etoileFill02");
+
+    gsap.to(element2, {
+        x: -2000,
+        y: 1900,
+        duration: 4,
+        delay: 3,
+        repeatDelay: 5,
+        repeat: -1,
+
+
+        onRepeat: () => {
+
+        },
+    })
+    const element3 = document.querySelector(".etoileFill03");
+
+    gsap.to(element3, {
+        x: -2000,
+        y: 1900,
+        duration: 2,
+        delay: 4,
         repeatDelay: 5,
         repeat: -1,
 
