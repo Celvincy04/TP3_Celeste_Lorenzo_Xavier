71910ba8042e46d578b6560319e1a7fc7ac6d872
Celvincy04 <2438710@bdeb.qc.ca>
2025-12-11 14:30:56 -0500

    Encore petit changement en pour mise en ligne

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/TP3_Celeste_Lorenzo_Xavier',
+    base: '/TP3_Celeste_Lorenzo_Xavier/',
     build: {
         emptyOutDir: true,
         rollupOptions: {
             input: {
                 main: 'index.html',
-                about: 'about.html',
             },
         },
     },
-})
+});
